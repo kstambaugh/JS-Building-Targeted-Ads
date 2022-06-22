ca3d30d3726180fc482045922830d6c92c742771
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-06-21 21:14:39 -0400

    almost done

--- public/app.js
+++ public/app.js
@@ -4,6 +4,19 @@
 
 // get user's time
 
+function userTime() {
+    const now = new Date()
+    return now.getHours()
+}
+
+function getMealTime() {
+    const tod = userTime()
+    return tod > 20 ? 'latenight snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}
+console.log(getMealTime())
+
+
+
 
 // helper functions
 // check time of day
@@ -12,6 +25,15 @@
 // build ads
 // build ad 1
 
+const buildAd1 = () => {
+    const mealTime = getMealTime();
+    let content = document.querySelector('.ad1');
+    let inner = document.createElement('p');
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`;
+    content.append(inner)
+}
+
+buildAd1()
 
 // build ad 2
 
